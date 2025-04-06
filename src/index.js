3be015823e4b2cbf8e68bb15c6cb39f2f629f589
M. S. Rihan <support@afabra.store>
2025-04-06 13:36:37 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
+// Global Styles
 import './styles/global.css';
 
-// Import Font Awesome for icons
-import '@fortawesome/fontawesome-free/css/all.min.css';
+// Font Awesome Setup
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fas, fab);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
