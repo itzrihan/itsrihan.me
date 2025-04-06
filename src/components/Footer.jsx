b78e9a6cd6790daa1da0ebbaf13afeb94da50e35
M. S. Rihan <support@afabra.store>
2025-04-06 18:55:19 +0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
-        <footer className="mt-[-1px] bg-[#00FF66] text-white text-center">
+        <footer className="mt-[-1px] bg-[#0d1117] text-[#8b949e] text-center border-t border-[#30363d]">
             <div className="py-10 px-4 md:px-6 text-sm md:text-lg">
-                <div className="max-w-screen-xl mx-auto border-t border-[#00AA44]">
+                <div className="max-w-screen-xl mx-auto">
                     <p className="pt-6">&copy; {(new Date()).getFullYear()} Rihan. All rights reserved.</p>
                 </div>
             </div>
