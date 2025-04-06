1ba10f40aa45721e03ea2d6fa7bb6d63c2539288
M. S. Rihan <support@afabra.store>
2025-04-06 18:59:01 +0600

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -18,15 +18,15 @@ const HeroSection = () => {
   }, [state]);
 
   return (
-    <section id="" className="bg-[#003311] relative">
+    <section id="" className="bg-[#0d1117] relative">
       <section>
         <div className="relative z-10 max-w-screen-xl mx-auto px-4 pt-28 md:px-8 md:pt-36 pb-14">
           <div className="space-y-5 max-w-4xl mx-auto text-center">
-            <h2 className="text-4xl text-white font-extrabold mx-auto md:text-5xl">
+            <h2 className="text-4xl text-[#c9d1d9] font-extrabold mx-auto md:text-5xl">
               <span>
                 Hey,<span className="hand">👋</span> I&apos;m{" "}
               </span>
-              <span className="text-[#00FF66] leading-normal">Rihan</span>
+              <span className="text-[#58a6ff] leading-normal">Rihan</span>
               <br />
               <Typewriter
                 onInit={(typewriter) => {
@@ -35,14 +35,14 @@ const HeroSection = () => {
                     .start();
                 }}
                 options={{
-                  wrapperClassName: "text-white",
-                  cursorClassName: "text-[#00FF66]"
+                  wrapperClassName: "text-[#c9d1d9]",
+                  cursorClassName: "text-[#58a6ff]"
                 }}
               />
             </h2>
-            <p className="max-w-2xl mx-auto text-gray-300 text-lg">
+            <p className="max-w-2xl mx-auto text-[#8b949e] text-lg">
               I'm not sure what's faster, debugging or making a cup of coffee.{" "}
-              <span className="text-[#00FF66] underline">
+              <span className="text-[#58a6ff] underline">
                 &apos;bug-free latte&apos;!
               </span>
             </p>
@@ -52,10 +52,10 @@ const HeroSection = () => {
           className="absolute inset-0 m-auto max-w-xs h-[357px] blur-[118px] sm md"
           style={{
             background:
-              "linear-gradient(106.89deg, rgba(0, 255, 102, 0.11) 15.73%, rgba(0, 170, 68, 0.41) 15.74%, rgba(0, 255, 102, 0.26) 56.49%, rgba(0, 170, 68, 0.4) 115.91%)",
+              "linear-gradient(106.89deg, rgba(88, 166, 255, 0.11) 15.73%, rgba(88, 166, 255, 0.41) 15.74%, rgba(88, 166, 255, 0.26) 56.49%, rgba(88, 166, 255, 0.4) 115.91%)",
           }}
         ></div>
-        <div className="absolute bottom-0 left-0 w-full h-32 bg-gradient-to-b from-transparent to-[#003311]"></div>
+        <div className="absolute bottom-0 left-0 w-full h-32 bg-gradient-to-b from-transparent to-[#0d1117]"></div>
       </section>
     </section>
   );
