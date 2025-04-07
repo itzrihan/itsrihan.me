4b91f8be4ef647b752c1c0f75accb63f70009aed
M. S. Rihan <support@afabra.store>
2025-04-07 19:48:36 +0600

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,86 +1,56 @@
-import styled from "styled-components";
-import { skills } from "../data/constant.js";
-import { Tilt } from "react-tilt";
+import { features } from '../data/constant';
 
-const SkillsContainer = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  justify-content: center;
-  @media (max-width:768px){   
-    grid-template-columns: repeat(1, 1fr);
-  }
-`;
-
-const Skill = styled.div`
-  width: 100%;
-  background-color: rgba(0, 51, 17, 0.83);
-  border: 1px solid rgba(0, 255, 102, 0.25);
-  box-shadow: rgba(0, 255, 102, 0.15) 0px 4px 24px;
-  border-radius: 16px;
-  padding: 18px 36px;
-  @media (max-width: 768px) {
-    padding: 20px 36px;
-  }
-  @media (max-width: 500px) {
-    padding: 20px 20px;
-  }
-`;
-
-const SkillItem = styled.div`
-  font-size: 16px;
-  font-weight: 400;
-  color: #ffffff;
-  border: 1px solid rgba(0, 255, 102, 0.5);
-  border-radius: 12px;
-  padding: 12px 16px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 8px;
-  background-color: rgba(0, 34, 17, 0.5);
-  &:hover {
-    background-color: rgba(0, 255, 102, 0.2);
-  }
-`;
-
-const SkillImage = styled.img`
-  width: 24px;
-  height: 24px;
-`;
-
-const Skills = () => {
+const Project = () => {
     return (
-        <section id="skills" className="mt-[-1px] relative py-5 bg-[#003311]">
+        <section id="project" className="mt-[-1px] relative py-12 bg-[#003311]">
             <div className="absolute top-0 left-0 w-full h-32 bg-gradient-to-t from-transparent to-[#003311]"></div>
-            <div className="absolute inset-0 max-w-md mx-auto h-72 blur-[118px]" style={{ background: "linear-gradient(152.92deg, rgba(0, 255, 102, 0.2) 4.54%, rgba(0, 170, 68, 0.26) 34.2%, rgba(0, 255, 102, 0.1) 77.55%)" }}></div>
             <div className="max-w-screen-xl mx-auto px-4 text-center text-gray-300 md:px-8">
                 <div className="max-w-3xl mx-auto relative">
-                    <h3 className="text-[#00FF66] text-5xl font-bold max-sm:text-3xl">
-                        Skills I Have
+                    <h3 className="text-white text-5xl font-semibold max-sm:text-3xl relative z-20">
+                        Still Working <span className="text-[#00FF66]">On projects</span>
                     </h3>
-                    <h1 className="mt-5 text-lg relative z-20">A glimpse into my expertise</h1>
+                    <p className="mt-5 text-lg relative z-20">Soon It's Gonna Complete </p>
                 </div>
-                <SkillsContainer className="mt-12 gap-10 max-lg:gap-5 max-sm:py-1">
-                    {skills.map((skill, index) => (
-                        <Tilt key={`skill-tilt-${index}`} className="flex">
-                            <Skill key={`skill-${index}`}>
-                                <h4 className="text-lg text-white font-semibold">{skill.title}</h4>
-                                <div className="grid grid-cols-3 gap-5 mt-5 max-lg:grid-cols-2 max-md:grid-cols-3 max-[580px]:grid-cols-2">
-                                    {skill.skills.map((item, index_x) => (
-                                        <SkillItem key={`skill-item-${index_x}`} className="text-[#c9d1d9]">
-                                            <SkillImage src={item.image} alt={item.name} />
-                                            {item.name}
-                                        </SkillItem>
-                                    ))}
-                                </div>
-                            </Skill>
-                        </Tilt>
-                    ))}
-                </SkillsContainer>
+                <div className="mt-12">
+                    <ul className="grid gap-y-8 gap-x-12 sm:grid-cols-2 lg:grid-cols-3">
+                        {features.map((item, idx) => (
+                            <li
+                                key={idx}
+                                className="px-8 py-5 hover:scale-105 transition-transform duration-300"
+                                style={{
+                                    backgroundColor: "rgba(0, 51, 17, 0.5)",
+                                    backdropFilter: "blur(16px) saturate(180%)",
+                                    WebkitBackdropFilter: "blur(16px) saturate(180%)",
+                                    border: "1px solid rgba(0, 255, 102, 0.25)",
+                                    boxShadow: "rgba(0, 255, 102, 0.15) 0px 4px 10px",
+                                    borderRadius: "12px",
+                                }}
+                            >
+                                <a href={item.link} className="flex h-full w-full flex-col gap-y-2">
+                                    <div className="w-full h-[180px] mx-auto text-[#00FF66] rounded-full flex items-center justify-center">
+                                        <img src={item.icon} alt="" className='w-full h-full object-cover'/>
+                                    </div>
+                                    <h4 className="text-lg text-white font-semibold">{item.title}</h4>
+                                    <p className="text-left text-gray-300">{item.desc}</p>
+                                    <ul className="flex flex-wrap justify-start items-center gap-2 mt-4">
+                                        {item.techs.map((tech, index) => (
+                                            <li
+                                                key={index}
+                                                className="p-2 bg-[#21262d] text-[#c9d1d9] rounded-lg border border-[#30363d]"
+                                            >
+                                                {tech}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
+            <div className="absolute inset-0 max-w-md mx-auto h-72 blur-[118px]" style={{ background: "linear-gradient(152.92deg, rgba(0, 255, 102, 0.2) 4.54%, rgba(0, 170, 68, 0.26) 34.2%, rgba(0, 255, 102, 0.1) 77.55%)" }}></div>
         </section>
     );
 };
 
-export default Skills;
+export default Project;
